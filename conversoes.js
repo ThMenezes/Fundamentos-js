6cc46acb5f192be8fa29f2d835ab33715d03238c
Desafio1 <tihta.meneze24@gmail.com>
2022-10-04 21:15:41 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleans
+
+// conversão inplícita
+const numero = 456;
+const numeroString = Number ("456a");
+
+//Number()
+// String()
+console.log(numero + numeroString)
+
+// conversão explícita
